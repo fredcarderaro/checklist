a31afa4bd2f95511251344ed7356b4c7741270c3
Fred Carderaro <fredcarderaro@gmail.com>
2026-05-03 14:53:39 -0300

    useState control todolist and implemente toggle check and delete todo item

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { ToDoItem } from "./components/ToDoItem";
 import { ToDoList } from "./components/ToDoList";
 import { TodoForm } from "./components/TodoForm";
 
-const todos = [
+/* const todos = [
   {
     id: 1,
     description: "JSX e componentes",
@@ -38,6 +38,7 @@ const todos = [
     createdAt: "2022-10-31",
   },
 ];
+
 const completed = [
   {
     id: 5,
@@ -52,17 +53,80 @@ const completed = [
     createdAt: "2022-10-31",
   },
 ];
+ */
 
 function App() {
+  // Cria estado de controle de exibição do modal Dialog
   const [showDialog, setShowDialog] = useState(false);
 
+  // Cria estado com lista de todos inicial
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      description: "JSX e componentes",
+      completed: false,
+      createdAt: "2022-10-31",
+    },
+    {
+      id: 2,
+      description: "Props, state e hooks",
+      completed: true,
+      createdAt: "2022-10-31",
+    },
+    {
+      id: 3,
+      description: "Ciclo de vida dos componentes",
+      completed: false,
+      createdAt: "2022-10-31",
+    },
+  ]);
+
+  // Exibir ou ocultar modal Dialog
   const toggleDialog = () => {
     setShowDialog(!showDialog);
   };
 
-  const addTodo = () => {
-    console.log("form submetido");
+  // Adicionar todo à lista
+  const addTodo = (formData) => {
+    const description = formData.get("description");
+
+    setTodos((prevState) => {
+      const todo = {
+        id: prevState.length + 1,
+        description,
+        completed: false,
+        createdAt: new Date().toISOString(),
+      };
+
+      return [...prevState, todo];
+    });
+
     toggleDialog();
+  };
+
+  // Funcao executa o toggle no item todo completed
+  const toggleTodoCompleted = (todo) => {
+    setTodos((prevState) => {
+      return prevState.map((item) => {
+        if (item.id == todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+
+        return item;
+      });
+    });
+  };
+
+  // Funcao exclui item todo
+  const deleteTodo = (todo) => {
+    setTodos((prevState) => {
+      return prevState.filter((item) => {
+        return item.id != todo.id;
+      });
+    });
   };
 
   return (
@@ -77,15 +141,33 @@ function App() {
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
-            {todos.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />;
-            })}
+            {todos
+              .filter((task) => !task.completed)
+              .map(function (task) {
+                return (
+                  <ToDoItem
+                    key={task.id}
+                    item={task}
+                    onToggleCompleted={toggleTodoCompleted}
+                    onDeleteTodo={deleteTodo}
+                  />
+                );
+              })}
           </ToDoList>
           <SubHeading>Concluído</SubHeading>
           <ToDoList>
-            {completed.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />;
-            })}
+            {todos
+              .filter((task) => task.completed)
+              .map(function (task) {
+                return (
+                  <ToDoItem
+                    key={task.id}
+                    item={task}
+                    onToggleCompleted={toggleTodoCompleted}
+                    onDeleteTodo={deleteTodo}
+                  />
+                );
+              })}
           </ToDoList>
           <Footer>
             {/* Dialog */}
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -5,7 +5,11 @@ import { TextInput } from "../TextInput";
 export function TodoForm({ onSubmit }) {
   return (
     <form action={onSubmit} className="todo-form">
-      <TextInput placeholder="Digite o item que deseja adicionar" required />
+      <TextInput
+        placeholder="Digite o item que deseja adicionar"
+        name="description"
+        required
+      />
       <Button>Salvar item</Button>
     </form>
   );
