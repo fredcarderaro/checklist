f4e2dc645e5dc2ced694b494e1ca138238a05a07
Fred Carderaro <fredcarderaro@gmail.com>
2026-05-04 19:48:06 -0300

    Implementação do Todo Context e refatoração da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { TodoProvider } from "./components/TodoProvider/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* TodoProvider importa o contexto do Todo criado e envolve toda a aplicação */}
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </StrictMode>,
-)
+);
